577c1cf42af6b70a2818b6a5d1eca1978788f3f1
Charlie Knight <williamchknight@gmail.com>
2020-11-05 22:41:14 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,8 +1,8 @@
 def find_max_value(array)
   count = 0 
-  max_value = nil
+  max_value = -1
   while count < array.length do
-    if array[count] > array[count + 1]
+    if max_value < array[count]
       max_value = array[count]
     end
     count += 1
